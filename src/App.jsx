aee2e44ccf6eba38ab940ccb88d8f3d0d23a16dd
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-05-01 13:02:04 +0300

    task 88 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Progress from "./components/Progress.jsx";
 
 class App extends React.Component {
   render() {
-    return <></>;
+    return (
+      <>
+        <Progress percentage={40} />
+      </>
+    );
   }
 }
 export default App;
