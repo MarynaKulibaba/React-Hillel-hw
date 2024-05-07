50b0c5129e0a464df9990ddf87f64f185c9a5a2b
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-05-07 21:41:46 +0300

    task 91 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Slider from "./components/Slider.jsx";
+import imgFallout from "./assets/fallout_wanderer.png";
+import imgRobot from "./assets/robot.png";
+import imgArmor from "./assets/body_armor.png";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Ініціалізація масиву зображень як властивість екземпляру класу
+    this.images = [imgFallout, imgRobot, imgArmor];
+  }
+
   render() {
     return (
       <>
-        <Slider />
+        <Slider images={this.images} />
       </>
     );
   }
 }
+
 export default App;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,49 +1,84 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlideIndex: 0,
+    };
+  }
+
+  prevSlide = () => {
+    this.setState((prevState) => ({
+      currentSlideIndex:
+        prevState.currentSlideIndex > 0
+          ? prevState.currentSlideIndex - 1
+          : this.props.images.length - 1,
+    }));
+  };
+  nextSlide = () => {
+    this.setState((prevState) => ({
+      currentSlideIndex:
+        prevState.currentSlideIndex < this.props.images.length - 1
+          ? prevState.currentSlideIndex + 1
+          : 0,
+    }));
+  };
   render() {
+    const { images } = this.props; // Отримання зображень з пропсів
+    const { currentSlideIndex } = this.state;
     return (
-      <>
-        <div id="carousel" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img alt="" className="d-block w-100" src="d" />
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {images.map((img, index) => (
+            <div key={index} className="slide">
+              <img
+                src={images[currentSlideIndex]}
+                alt={`Slide ${currentSlideIndex}`}
+              />
             </div>
-            <div className="carousel-item">
-              <img alt="" className="d-block w-100" src="/images/second.jpeg" />
-            </div>
-            <div className="carousel-item">
-              <img alt="" className="d-block w-100" src="/images/third.jpeg" />
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            data-bs-target="#carousel"
-            type="button"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            data-bs-target="#carousel"
-            type="button"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          ))}
         </div>
-      </>
+
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+          onClick={this.prevSlide}
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+          onClick={this.nextSlide}
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     );
   }
 }
+
+Slider.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Slider;
