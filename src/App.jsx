e611fe1ef3c0bb5b7a2d9400622e8ebaca2f0030
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-05-14 17:05:40 +0300

    task 98 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import Card from "./components/Card";
 
-class App extends React.Component {
-  render() {
-    return <></>;
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Card>
+        <Card.Body>
+          <Card.Title>Title</Card.Title>
+          <Card.Text>Text</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
 export default App;
---
+++ src/components/Card/Body.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Body = ({ children }) => {
+  return <div className="card-body">{children}</div>;
+};
+
+Body.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Body;
