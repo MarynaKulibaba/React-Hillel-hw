a87d250a1f6d54df6de8a82fd9c01f5a165dfbf2
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-05-28 13:50:58 +0300

    task 100 completed

--- src/components/Albums/AlbumCatalog.jsx
+++ src/components/Albums/AlbumCatalog.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { Container, ListGroup, Alert } from "react-bootstrap";
 import AlbumItem from "./AlbumItem";
 import AlbumPhotos from "./AlbumPhotos";
